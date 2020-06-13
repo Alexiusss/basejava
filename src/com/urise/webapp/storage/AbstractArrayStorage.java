af5c52c9e5264a8fb1b59b90f5a9f6a20abaab02
Alexiuss <kuzma.prutckoff@yandex.ru>
2020-06-14 00:02:23 +0300

    HW3

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -10,20 +12,70 @@ public abstract class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);   // Метод fill заполняет массив выбранными значением
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            System.out.printf("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
     public int size() {
         return size;
     }
 
+    public void save(Resume r) {
+
+        int index = getIndex(r.getUuid());
+        if (index > 0) {
+            System.out.printf("Resume " + r.getUuid() + " already exist");
+        } else if (size == STORAGE_LIMIT) {
+            System.out.printf("Storage overflow");
+        } else {
+            insertElement(r, index);
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.printf("Resume " + uuid + " not exist");
+        } else {
+            fillDeleteElement(index);
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+
+
     public Resume get(String uuid) {
         int index = getIndex(uuid);
-        if (index == -1) {
+        if (index < 0) {
             System.out.printf("Resume " + uuid + " not exist");
             return null;
         }
         return storage[index];
     }
 
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+
+    }
+
     protected abstract int getIndex(String uuid);
 
+    protected abstract void fillDeleteElement(int index);
+
+    protected abstract void insertElement(Resume r, int index);
 
 }
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,60 +2,20 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);   // Метод fill заполняет массив выбранными значением
-        size = 0;
+    @Override
+    protected void fillDeleteElement(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index != -1) {
-            System.out.printf("Resume " + r.getUuid() + " not exist");
-        } else {
-            storage[index] = r;
-        }
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
     }
-
-    public void save(Resume r) {
-        if (getIndex(r.getUuid()) != -1) {
-            System.out.printf("Resume " + r.getUuid() + " already exist");
-        } else if (size == STORAGE_LIMIT) {
-            System.out.printf("Storage overflow");
-        } else {
-            storage[size] = r;
-            size++;
-        }
-    }
-
-
-    public void delete(String uuid) {
-
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.printf("Resume " + uuid + " not exist");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-
-    }
-
 
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
