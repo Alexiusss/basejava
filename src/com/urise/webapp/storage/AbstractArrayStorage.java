05b666042037a0ad450d005d6723efd9ac992147
Alexiuss <kuzma.prutckoff@yandex.ru>
2020-06-18 21:45:41 +0300

    HW4

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -1,8 +1,5 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.exception.ExistStorageException;
-import com.urise.webapp.exception.NotExistStorageException;
-import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
@@ -10,7 +7,7 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public abstract class AbstractArrayStorage implements Storage {
+public abstract class AbstractArrayStorage extends AbstractStorage {
     protected static final int STORAGE_LIMIT = 100000;
     protected Resume[] storage;
     protected int size = 0;
@@ -19,67 +16,18 @@ public abstract class AbstractArrayStorage implements Storage {
         this.storage = new Resume[STORAGE_LIMIT];
     }
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);   // Метод fill заполняет массив выбранными значением
-        size = 0;
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index < 0) {
-            throw new NotExistStorageException(r.getUuid());
-        } else {
-            storage[index] = r;
-        }
-    }
-
     public int size() {
         return size;
     }
 
-    public void save(Resume r) {
-
-        int index = getIndex(r.getUuid());
-        if (index >= 0) {
-            throw new ExistStorageException(r.getUuid());
-        } else if (size == STORAGE_LIMIT) {
-            throw new StorageException("Storage overflow", r.getUuid());
-        } else {
-            insertElement(r, index);
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-
-        int index = getIndex(uuid);
-        if (index < 0) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            fillDeleteElement(index);
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            throw new NotExistStorageException(uuid);
-        }
-        return storage[index];
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);   // Метод fill заполняет массив выбранными значением
+        size = 0;
     }
 
     public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
 
     }
-
-    protected abstract int getIndex(String uuid);
-
-    protected abstract void fillDeleteElement(int index);
-
-    protected abstract void insertElement(Resume r, int index);
 
 }
