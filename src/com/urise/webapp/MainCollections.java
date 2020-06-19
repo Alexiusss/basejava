afc0014933e8ffd67ad14935b70a7eeede13f086
Alexiuss <kuzma.prutckoff@yandex.ru>
2020-06-19 21:32:09 +0300

    HW5

--- src/com/urise/webapp/MainCollections.java
+++ src/com/urise/webapp/MainCollections.java
@@ -51,8 +51,14 @@ if(iterator.next().getUuid().equals(UUID_1)){
         map.put(UUID_3, RESUME_3);
 
         for (Map.Entry<String, Resume> entry: map.entrySet()){
-
+            System.out.println(entry.getValue());
         }
+
+
+        List<Resume> resumes = Arrays.asList(RESUME_1, RESUME_2, RESUME_3);
+
+        resumes.remove(1);
+        System.out.println(resumes);
 
     }
 
