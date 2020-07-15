103b6bfd8f789263737bdb64bdafcdb45012a3e9
Alexiuss <kuzma.prutckoff@yandex.ru>
2020-07-15 21:21:27 +0300

    HW 13.2

--- config/populate.sql
+++ config/populate.sql
@@ -1,5 +1,4 @@
 INSERT INTO resume (uuid, full_name)
-VALUES ('uuid1', 'Name1'),
-       ('uuid2', 'Name2'),
-       ('uuid3', 'Name3'),
-       ('uuid4', 'Name4');
+VALUES ('a8b72935-f11d-4f63-969f-98df1d397768', 'Name1'),
+       ('8c87f14e-bb48-42ee-83c5-8308f5c56ac9', 'Name2'),
+       ('879464e5-c180-460a-9bac-7c4ecd582ede', 'Name3');
